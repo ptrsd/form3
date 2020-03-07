f326dc944e44b98b9d9927067fbb71aff1cd2b3e
Piotr Siudy <github@siudy.info>
2020-03-07 20:21:09 +0100

    new request wrapper with JSON handling. "do" method with JSON body handling. Tests.

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package form3
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/url"
 )
@@ -9,6 +11,7 @@ const (
 	version          = "v1"
 	defaultUserAgent = "form3-client/" + version
 	defaultBaseURL   = "http://localhost:8080/"
+	contentType      = "application/vnd.api+json"
 )
 
 type Client struct {
@@ -27,4 +30,41 @@ func NewDefaultClient(httpClient *http.Client) (client *Client) {
 	client = &Client{httpClient: httpClient, BaseURL: baseURL, UserAgent: defaultUserAgent}
 
 	return client
+}
+
+func (c *Client) newRequest(method, path string, body interface{}) (req *http.Request, err error) {
+	reqURL := c.BaseURL.ResolveReference(&url.URL{Path: path})
+	buf := bytes.Buffer{}
+
+	if body != nil {
+		encoder := json.NewEncoder(&buf)
+		if err = encoder.Encode(body); err != nil {
+			return nil, err
+		}
+	}
+
+	req, err = http.NewRequest(method, reqURL.String(), &buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", contentType)
+	}
+
+	req.Header.Set("Accept", contentType)
+	req.Header.Set("User-Agent", defaultUserAgent)
+
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request, respType interface{}) (*http.Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(respType)
+	return resp, err
 }
--- client_test.go
+++ client_test.go
@@ -1,12 +1,16 @@
 package form3
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewDefaultClient(t *testing.T) {
+func Test_NewDefaultClient(t *testing.T) {
 	client := NewDefaultClient(nil)
 
 	require.NotNil(t, client)
@@ -14,4 +18,78 @@ func TestNewDefaultClient(t *testing.T) {
 
 	require.Equal(t, "http://localhost:8080/", client.BaseURL.String())
 	require.Equal(t, "form3-client/v1", client.UserAgent)
+}
+
+func Test_whenRequestWithoutBodyThenContentTypeNotSet(t *testing.T) {
+	client := testClient("")
+
+	request, err := client.newRequest(http.MethodGet, "/", nil)
+	require.Nil(t, err)
+	require.Equal(t, request.Header.Get("Content-Type"), "")
+}
+
+func Test_whenRequestWithBodyThenContentTypeIsSet(t *testing.T) {
+	client := testClient("")
+
+	request, err := client.newRequest(http.MethodGet, "/", "")
+	require.Nil(t, err)
+	require.Equal(t, request.Header.Get("Content-Type"), contentType)
+}
+
+func Test_whenRequestWithInvalidBodyThenReturnError(t *testing.T) {
+	client := testClient("")
+
+	req, err := client.newRequest(http.MethodGet, "/", make(chan int))
+	require.NotNil(t, err, err.Error())
+	require.Nil(t, req)
+}
+
+func Test_whenCallingExistingServiceThenFillBody(t *testing.T) {
+	server := startServer()
+	client := testClient(server.URL)
+	defer server.Close()
+
+	req, err := client.newRequest(http.MethodGet, "/", nil)
+	require.Nil(t, err)
+	require.NotNil(t, req)
+
+	resBody := &struct {
+		Status string `json:"status"`
+	}{}
+
+	_, err = client.do(req, resBody)
+	require.Nil(t, err)
+	require.Equal(t, resBody.Status, "success")
+}
+
+func Test_whenCallingNotExistingServiceThenReturnError(t *testing.T) {
+	client := testClient("http://not-existing-service")
+
+	req, err := client.newRequest(http.MethodGet, "/", nil)
+	require.Nil(t, err)
+	require.NotNil(t, req)
+
+	resBody := &struct {
+		Status string `json:"status"`
+	}{}
+
+	_, err = client.do(req, resBody)
+	require.NotNil(t, err)
+}
+
+func testClient(addr string) *Client {
+	testURL, _ := url.Parse(addr)
+	client := &Client{
+		httpClient: http.DefaultClient,
+		BaseURL:    testURL,
+		UserAgent:  "",
+	}
+
+	return client
+}
+
+func startServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `{"status":"success"}`)
+	}))
 }
