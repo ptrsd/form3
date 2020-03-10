b357e615b9bdbb390e6c49576fac288019529a7c
Piotr Siudy <github@siudy.info>
2020-03-10 21:23:07 +0100

    remove external libraries

--- client_test.go
+++ client_test.go
@@ -5,76 +5,114 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
+
+type assertion struct {
+	expected interface{}
+	actual   interface{}
+	name     string
+}
+
+type assertions []assertion
 
 func Test_NewDefaultClient(t *testing.T) {
 	client := NewDefaultClient(nil)
 
-	require.NotNil(t, client)
-	require.NotNil(t, client.httpClient)
+	notNils := assertions{
+		{actual: client, name: "Client"},
+		{actual: client.httpClient, name: "Client.httpClient"},
+	}
+	assertNotNil(t, notNils)
 
-	require.Equal(t, "http://localhost:8080/", client.BaseURL.String())
-	require.Equal(t, "form3-client/v1", client.UserAgent)
+	equals := assertions{
+		{actual: client.BaseURL.String(), expected: defaultBaseURL, name: "Client.BaseURL"},
+		{actual: client.UserAgent, expected: defaultUserAgent, name: "Client.UserAgent"},
+	}
+	assertEquals(t, equals)
 }
 
-func Test_whenRequestWithoutBodyThenContentTypeNotSet(t *testing.T) {
+func Test_whenRequestWithoutBodyThenContentTypeIsNotSet(t *testing.T) {
 	client := testClient("")
 
 	request, err := client.newRequest(http.MethodGet, "/", nil)
-	require.Nil(t, err)
-	require.Equal(t, request.Header.Get("Content-Type"), "")
+	if err != nil {
+		t.Errorf("error while creating new request, %s", err.Error())
+	}
+
+	assertEquals(t, assertions{
+		{actual: request.Header.Get("Content-Type"), expected: "", name: "Client.ContentType"},
+	})
 }
 
 func Test_whenRequestWithBodyThenContentTypeIsSet(t *testing.T) {
 	client := testClient("")
 
-	request, err := client.newRequest(http.MethodGet, "/", "")
-	require.Nil(t, err)
-	require.Equal(t, request.Header.Get("Content-Type"), contentType)
+	request, err := client.newRequest(http.MethodPost, "/", "")
+	if err != nil {
+		t.Errorf("error while creating new request, %s", err.Error())
+	}
+
+	assertEquals(t, assertions{
+		{actual: request.Header.Get("Content-Type"), expected: contentType, name: "Client.ContentType"},
+	})
 }
 
 func Test_whenRequestWithInvalidBodyThenReturnError(t *testing.T) {
 	client := testClient("")
 
-	req, err := client.newRequest(http.MethodGet, "/", make(chan int))
-	require.NotNil(t, err, err.Error())
-	require.Nil(t, req)
+	_, err := client.newRequest(http.MethodGet, "/", make(chan int))
+	assertNotNil(t, assertions{
+		{actual: err, name: "Client.InvalidBodyError"},
+	})
+
+	assertEquals(t, assertions{
+		{actual: err.Error(), expected: "json: unsupported type: chan int", name: "Client.InvalidBodyErrorMessage"},
+	})
 }
 
-func Test_whenCallingExistingServiceThenFillBody(t *testing.T) {
+func Test_whenCallingExistingServiceThenReturnBody(t *testing.T) {
 	server := startServer()
-	client := testClient(server.URL)
 	defer server.Close()
 
+	client := testClient(server.URL)
+
 	req, err := client.newRequest(http.MethodGet, "/", nil)
-	require.Nil(t, err)
-	require.NotNil(t, req)
+	if err != nil {
+		t.Errorf("error while creating new request, %s", err.Error())
+	}
 
 	resBody := &struct {
 		Status string `json:"status"`
 	}{}
 
-	_, err = client.do(req, resBody)
-	require.Nil(t, err)
-	require.Equal(t, resBody.Status, "success")
+	err = client.do(req, resBody)
+	if err != nil {
+		t.Errorf("error while calling service, %s", err.Error())
+	}
+
+	assertEquals(t, assertions{
+		{actual: resBody.Status, expected: "success", name: "Client.ResponseBody"},
+	})
 }
 
 func Test_whenCallingNotExistingServiceThenReturnError(t *testing.T) {
 	client := testClient("http://not-existing-service")
 
 	req, err := client.newRequest(http.MethodGet, "/", nil)
-	require.Nil(t, err)
-	require.NotNil(t, req)
+	if err != nil {
+		t.Errorf("error while creating new request, %s", err.Error())
+	}
 
 	resBody := &struct {
 		Status string `json:"status"`
 	}{}
 
-	_, err = client.do(req, resBody)
-	require.NotNil(t, err)
+	err = client.do(req, resBody)
+	assertNotNil(t, assertions{
+		{actual: err, name: "Client.CallingNotExistingService"},
+	})
 }
 
 func testClient(addr string) *Client {
@@ -92,4 +130,28 @@ func startServer() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, `{"status":"success"}`)
 	}))
+}
+
+func assertNotNil(t *testing.T, notNils assertions) {
+	for _, assertion := range notNils {
+		if assertion.actual == nil {
+			t.Errorf("%s: Expected not to be nil", assertion.name)
+		}
+	}
+}
+
+func assertEquals(t *testing.T, equals assertions) {
+	for _, assertion := range equals {
+		if !reflect.DeepEqual(assertion.expected, assertion.actual) {
+			t.Errorf("%s: Expected: %v, Actual: %v", assertion.name, assertion.expected, assertion.actual)
+		}
+	}
+}
+
+func assertNotEmpty(t *testing.T, notEmpty []assertion) {
+	for _, assertion := range notEmpty {
+		if assertion.actual == reflect.Zero(reflect.TypeOf(assertion.actual)).Interface() {
+			t.Errorf("%s: Expected not to be empty", assertion.name)
+		}
+	}
 }
